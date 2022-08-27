19b95b952d6cbc88d0c5736a633d73192e35c182
Tarek <83262890+tarek102@users.noreply.github.com>
2022-08-27 23:10:49 +0200

    Edit Navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IoIosPerson } from 'react-icons/io';
 
 const Navbar = () => (
   <div className="navbar">
@@ -8,6 +9,7 @@ const Navbar = () => (
         <li><h2 className="logo">Bookstore CMS</h2></li>
         <li><Link to="/">Books</Link></li>
         <li className="category"><Link to="/categories">Categories</Link></li>
+        <li className="user-img"><IoIosPerson /></li>
       </ul>
     </nav>
   </div>
